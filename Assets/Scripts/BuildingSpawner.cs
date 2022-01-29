e56c880af668be37f5c4ea49f649737cf3f4a2c5
Fzhut0 <80861162+Fzhut0@users.noreply.github.com>
2022-01-29 02:25:57 +0100

    Added random rotation to spawner

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -11,29 +11,31 @@ public class BuildingSpawner : MonoBehaviour
 
     private void Start()
     {
-
+        /*
         for (int i = 0; i < buildingAmount; i++)
+        {
+            SpawnBuilding();
+        }
+        */
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.G))
         {
             SpawnBuilding();
         }
 
     }
 
-    private void Update()
-    {
-
-        /* if (Input.GetKey(KeyCode.G))
-         {
-             SpawnBuilding();
-         }
-        */
-    }
-
     void SpawnBuilding()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(50, 900f), 0, Random.Range(50, 900f));
+        float terHeight = Terrain.activeTerrain.SampleHeight(transform.localPosition);
+        Vector3 spawnPos = new Vector3(Random.Range(200, 800), terHeight, Random.Range(200, 900));
+        Quaternion spawnRotation = Quaternion.Euler(0, Random.Range(0, 90), 0);
 
-        Instantiate(buildingSpawn[Random.Range(0, buildingSpawn.Count)], spawnPos, Quaternion.identity);
+        Instantiate(buildingSpawn[Random.Range(0, buildingSpawn.Count)], spawnPos, spawnRotation);
 
 
     }
