be0d6f2aa5a6e6a60ec8e0e8ff16a01bbcf03200
Fzhut0 <80861162+Fzhut0@users.noreply.github.com>
2022-01-29 04:14:10 +0100

    Spawned object height is now properly adjusted to generated spawn point height

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -12,6 +12,8 @@ public class BuildingSpawner : MonoBehaviour
     private void Start()
     {
         /*
+         * actual code which will be used to spawn objects at the start of level
+         * 
         for (int i = 0; i < buildingAmount; i++)
         {
             SpawnBuilding();
@@ -22,6 +24,8 @@ public class BuildingSpawner : MonoBehaviour
     private void Update()
     {
 
+        // Placeholder function for testing purposes
+
         if (Input.GetKey(KeyCode.G))
         {
             SpawnBuilding();
@@ -31,12 +35,23 @@ public class BuildingSpawner : MonoBehaviour
 
     void SpawnBuilding()
     {
-        float terHeight = Terrain.activeTerrain.SampleHeight(transform.localPosition);
-        Vector3 spawnPos = new Vector3(Random.Range(200, 800), terHeight, Random.Range(200, 900));
-        Quaternion spawnRotation = Quaternion.Euler(0, Random.Range(0, 90), 0);
+        Vector3 spawnPos = new Vector3(Random.Range(200, 800), 100, Random.Range(200, 900));
+
+        Ray checkHeight = new Ray(spawnPos, Vector3.down);
+        RaycastHit actualHeight;
+
+        Physics.Raycast(checkHeight, out actualHeight, Mathf.Infinity);
+
+        float terHeight = Terrain.activeTerrain.SampleHeight(actualHeight.point) + transform.position.y;
+
+
+        spawnPos.y = terHeight;
+
+        Quaternion spawnRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
 
         Instantiate(buildingSpawn[Random.Range(0, buildingSpawn.Count)], spawnPos, spawnRotation);
 
-
     }
+
+
 }
